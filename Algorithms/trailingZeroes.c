92fbaeec8b579769d87993919c20cb6161706b7b
Zeng Zhongchao <zzchao03@163.com>
2015-08-05 16:49:54 +0800

    update trailingZeroes

--- Algorithms/trailingZeroes.c
+++ Algorithms/trailingZeroes.c
@@ -1,33 +1,31 @@
-/*
- * Given an integer n, return the number of trailing zeroes in n!.
- *
- * Note: Your solution should be in logarithmic time complexity.
- */
-// 这个版本在 leetcode 上运行会超时，算法太naiive。
-// 请参考本文件夹下的另一个版本
-int fac_by_2(int n){
-    int res=0;
-    while(n % 2 ==0){
-        res++;
-        n /= 2;
-    }
-   return res;
-}
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+// Given an integer n, return the number of trailing zeroes in n!.
+//
+// Note: Your solution should be in logarithmic time complexity.
 
-int fac_by_5(int n){
+#include <stdio.h>
+int trailingZeroes(int n) {
     int res=0;
-    while(n % 5 ==0){
-        res++;
-        n /= 5;
+    while((n /= 5) != 0){
+        res += n;
     }
     return res;
 }
- 
-int trailingZeroes(int n) {
-    int fac2=0, fac5=0;
-    for(int i=1; i<=n; i++){
-        fac2 += fac_by_2(i);
-        fac5 += fac_by_5(i);
-    }
-    return fac2<fac5 ? fac2 :fac5 ;
+
+int main(){
+    printf("n=10: %d\n", trailingZeroes(10));
+    printf("n=15: %d\n", trailingZeroes(15));
+    printf("n=25: %d\n", trailingZeroes(25));
+    printf("n=35: %d\n", trailingZeroes(35));
+    printf("n=1000: %d\n", trailingZeroes(1000));
+    return 0;
 }
+
+
+/*
+ * 算法：
+ * 由于2比5多，只需要count有多少个5。
+ * 在1～n 的数中，有 n/5 个数是5的倍数，这些数对5的指数贡献为1；
+ * 这些数中，又有 n/25 个数是25的倍数，每个这些数对5的指数贡献再+1；
+ * n/5^3 个数是 5^3 的倍数，以此类推。
+ */
